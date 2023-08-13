cb3b233f8ddabdd340a8fd2f799517f382b5ccd5
rabijati <jattirabi07@gmail.com>
2023-08-13 15:22:20 +0545

    convert to web using flask and added new files index.html

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import cv2
 import os
 import numpy as np
 from sklearn.model_selection import train_test_split
-
-import tensorflow as tf
+from flask import Flask, render_template, request
 from tensorflow.keras import layers, models
 
 
@@ -27,7 +26,6 @@ non_tiger_images, non_tiger_labels = load_and_preprocess_images(r"F:\CapstoneII\
 
 all_images = tiger_images + non_tiger_images
 all_labels = tiger_labels + non_tiger_labels
-
 
 X_train, X_test, y_train, y_test = train_test_split(all_images, all_labels, test_size=0.2, random_state=42)
 
@@ -56,19 +54,35 @@ y_test = np.array(y_test)
 test_loss, test_accuracy = model.evaluate(X_test, y_test)
 print("Test accuracy:", test_accuracy)
 
-
-def detect_tiger(image_path, model):
-    image = cv2.imread(image_path)
-    preprocessed_image = cv2.resize(image, (224, 224))
-
-    preprocessed_image = np.expand_dims(preprocessed_image, axis=0)
-    prediction = model.predict(preprocessed_image)[0][0]
-
-    if prediction > 0.5:
-        return "Tiger detected with confidence: {:.2f}".format(prediction)
-    else:
-        return "No tiger detected."
+app = Flask(__name__)  # Create a Flask app instance
 
 
-result = detect_tiger(r"F:\CapstoneII\tigerdetectionsystem\image\tiger3.jpg", model)
-print(result)
+# Route for handling the web application
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    try:
+        if request.method == "POST":
+            uploaded_file = request.files["file"]
+            if uploaded_file.filename != "":
+                image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), cv2.IMREAD_COLOR)
+                preprocessed_image = cv2.resize(image, (224, 224))
+                preprocessed_image = np.expand_dims(preprocessed_image, axis=0)
+                prediction = model.predict(preprocessed_image)[0][0]
+                if prediction > 0.5:
+                    result = "Tiger detected "
+                # result = "Tiger detected with confidence: {:.2f}".format(prediction)
+                else:
+                    result = "No tiger detected."
+                return render_template("index.html", result=result, image_uploaded=True)
+
+        return render_template("index.html", image_uploaded=False)
+
+    except Exception as e:
+        error_message = "An error occurred: {}".format(e)
+        return render_template("index.html", error_message=error_message, image_uploaded=False)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
